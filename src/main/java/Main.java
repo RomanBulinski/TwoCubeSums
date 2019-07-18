4973bcccef3776afe8fa76ab5d0c48b54daea6ff
Roman Bulinski <aboandrus@gmail.com>
2019-07-18 09:06:25 +0200

    Implemented app

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
     public static boolean hasTwoCubeSums(int n) {
 
+        int counter =0
+        int border = (int) Math.pow(n, 1.0 / 3);
+
+        List<Integer> numbers = new ArrayList<>();
+        for(int i =1; i <= border; i++){
+            numbers.add(i);
+        }
 
 
-        return  true;
+        for( int a = 0; a<numbers.size(); a++ ){
+            for( int b = a+1; b<numbers.size(); b++ ){
+                Double tempSum = Math.pow(numbers.get(a),3) + Math.pow(numbers.get(b),3);
+                if(tempSum==n){
+                    counter++;
+                }
+            }
+        }
+
+        if( counter>1 ){
+            return  true;
+        }
+        return  false;
     }
 
-    public static void main(String[] args){
 
-        System.out.println(   Math.pow(4105, 1.0 / 3) );
-
-    }
 }
